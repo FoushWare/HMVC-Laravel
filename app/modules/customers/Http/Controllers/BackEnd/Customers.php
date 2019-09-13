50ae17665b7e81c05252c6953c087e37c2698b36
foushware <lionmoon60@yahoo.com>
2019-09-13 23:58:13 +0200

    load the controllers of each module

---
+++ app/modules/customers/Http/Controllers/BackEnd/Customers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Customers\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+
+class Customers extends Controller
+{
+
+    public function index(){
+        dd("customers backend");
+    }
+}
---
+++ app/modules/customers/Http/Controllers/FrontEnd/Customers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Customers\Http\Controllers\FrontEnd;
+
+use App\Http\Controllers\Controller;
+
+class Customers extends Controller
+{
+
+    public function index(){
+        dd("customers frontend");
+    }
+}
